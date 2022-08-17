8b565c5bbd511e157501ed7f37b91ead1bfab360
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-17 09:07:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,29 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = ">= 2.13.0"
+      source = "kreuzwerker/docker"
+      version = "~> 2.15.0"
     }
   }
 }
- variable "reponame" {}
- variable "container_port" {}
+
+variable "reponame" {}
+variable "container_port" {}
 
 provider "docker" {}
 
-resource "docker_image" "juan" {
+resource "docker_image" "nginx" {
   name         = "nginx:latest"
-  keep_locally = false
+  keep_locally = true
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = var.reponame
+  name  = var.reponame   
   ports {
     internal = 80
-    external = var.container_port
+    external = var.container_port 
   }
 }
+
 // en terraformm para declarar variables se usa la palabra "variable" sin embargo para utilizarlas se utiliza el prefijo "var.(nombre de la variable)".
